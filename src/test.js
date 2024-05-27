16d47042dcd51c2a64e03bd16e85aff7ec8939a1
Agung Ramananda <agungramananda@gmail.com>
2024-05-27 13:30:32 +0800

    Mengubah skema

--- src/test.js
+++ src/test.js
@@ -1,14 +1,69 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const { flavorQuery } = require("./query/query.js");
 
-const filterByFlavor = async (request, h) => {
+const filterByFlavor = async () => {
   try {
-    const flavor = await prisma.iceCream.findMany(
-      flavorQuery("Classic Vanilla")
-    );
-    console.log(flavor);
-  } catch (error) {
+    const flavor = await prisma.iceCream.findMany({
+      include: {
+        size: true, // Menyertakan informasi ukuran
+        types: true, // Menyertakan informasi tipe
+        flavors: true, // Menyertakan informasi rasa
+        toppings: true, // Menyertakan informasi topping
+      },
+    });
+    flavor.forEach(async (iceCream) => {
+      const {
+        id,
+        name,
+        size_type,
+        price,
+        description,
+        images,
+        types,
+        flavors,
+        toppings,
+      } = iceCream;
+      const typeId = types.map((type) => type.type_id);
+      const type = await prisma.type.findUnique({
+        where: {
+          id: typeId[0],
+        },
+        select: {
+          types: true,
+        },
+      });
+      const flavorId = flavors.map((flavor) => flavor.flavor_id);
+      const flavor = await prisma.flavor.findUnique({
+        where: {
+          id: flavorId[0],
+        },
+        select: {
+          flavors: true,
+        },
+      });
+      const toppingId = toppings.map((topping) => topping.topping_id);
+      const topping = await prisma.topping.findUnique({
+        where: {
+          id: toppingId[0],
+        },
+        select: {
+          toppings: true,
+        },
+      });
+
+      console.log(`
+        ID: ${id}
+        Name: ${name}
+        Size: ${size_type}
+        Type: ${type.types}
+        Flavors: ${flavor.flavors}
+        Toppings: ${topping.toppings}
+        Price: ${price}
+        Description: ${description}
+        Images: ${images || "No image available"}
+      `);
+    });
+  } catch (e) {
     console.error(e);
   }
 };
